63587d2ba51e72fa9d5e36bea7cc3917c88e25a5
Shruti Taware <45720788+shrutitaware@users.noreply.github.com>
2021-08-31 23:46:25 -0700

    Update and rename Amazon-Window functions & aliasing.sql to Amazon-Window-functions-aliasing.sql

--- Amazon-Window-functions-aliasing.sql
+++ Amazon-Window-functions-aliasing.sql
@@ -1,6 +1,10 @@
+[YouTube - Nate at StrataScratch] (https://www.youtube.com/watch?app=desktop&v=QenwDm5oWdU&t=236s)
+[Coding Platform] (https://platform.stratascratch.com/coding/10319-monthly-percentage-difference?python&utm_source=youtube&utm_medium=click&utm_campaign=YT+description+link)
+/*
 # Monthly Percentage Difference
 
-Given a table of purchases by date, calculate the month-over-month percentage change in revenue. The output should include the year-month date (YYYY-MM) and percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
+Given a table of purchases by date, calculate the month-over-month percentage change in revenue. The output should include the year-month date (YYYY-MM) and 
+percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
 The percentage change column will be populated from the 2nd month forward and can be calculated as ((this month's revenue - last month's revenue) / last month's revenue)*100.
 
 
@@ -16,6 +20,7 @@ The percentage change column will be populated from the 2nd month forward and ca
 --b. total value for last month
 --c aggregated by yyyy-mm
 --d. round to 2nd decimal
+*/
 
 select  
   to_char(cast(created_at as date),'YYYY-MM') as year_month,
@@ -25,7 +30,3 @@ from sf_transactions
 group by year_month
 WINDOW w AS (order by to_char(cast(created_at as date),'YYYY-MM'))
 order by year_month
-
-
-[YouTube - Nate at StrataScratch] (https://www.youtube.com/watch?app=desktop&v=QenwDm5oWdU&t=236s)
-[Coding Platform] (https://platform.stratascratch.com/coding/10319-monthly-percentage-difference?python&utm_source=youtube&utm_medium=click&utm_campaign=YT+description+link)
